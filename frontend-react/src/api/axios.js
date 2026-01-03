fbdc5f71ff9a2290e6092dc6f2dfaa792144f2bf
samuel101-crypto <madehinsamuel@gmail.com>
2026-01-03 15:51:59 +0100

    updated baseURL

--- frontend-react/src/api/axios.js
+++ frontend-react/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://considerable-cathrin-wetalk-0d4f7320.koyeb.app/',
+    baseURL: 'https://considerable-cathrin-wetalk-0d4f7320.koyeb.app/api/v1/',
     headers: {
         'Content-Type': 'application/json',
     },
